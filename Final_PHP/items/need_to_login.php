e069777178e51efe5371488e02bf31a64e53945e
AlpetGexh <50520333+AlpetGexh@users.noreply.github.com>
2021-07-09 05:10:33 +0200

    Projects

--- Final_PHP/items/need_to_login.php
+++
@@ -1,9 +0,0 @@
-<?php 
-  if (!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn']==false) {
-  header("Location:login.php");
-  die();  
-}
-  /*
-  <?php include('../items/need_to_login.php');?>
-  */
-  ?>
--- Final_PHP/items/title_bar_img.php
+++
@@ -1,4 +0,0 @@
-<?php 
-echo("<link rel='shortcut icon' type='image/x-icon' href='../Final_PHP/img/Killerlogo.jpg'>")
-//<?php include('../Final_PHP/items/title_bar_img.php');
-?>
