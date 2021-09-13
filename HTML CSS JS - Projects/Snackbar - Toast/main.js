eab1808e8b8c2bdd3a1a6dcf93a5f021bbd23cc0
AlpetGexha <50520333+AlpetGexh@users.noreply.github.com>
2021-09-13 03:18:01 +0200

    Snackbar - Toast

---
+++ HTML CSS JS - Projects/Snackbar - Toast/main.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function () {
+        x.className = x.className.replace("show", "");
+    }, 4000); //sekondata
+}
