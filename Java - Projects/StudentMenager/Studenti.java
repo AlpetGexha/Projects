8f760b90da7cab99e3d0bcb4d359b6426488d5f2
AlpetGexha <50520333+AlpetGexh@users.noreply.github.com>
2021-11-23 23:41:12 +0100

    Update Studenti.java

--- Java - Projects/StudentMenager/Studenti.java
+++ Java - Projects/StudentMenager/Studenti.java
@@ -11,68 +11,116 @@ class Studenti {
 	// private static int _generateID = 1000;
 	private int _id;
 
+	/**
+	 * @param Jep flerën e ID-s ({@code int})
+	 */
 	protected void setId(int value) {
 //		_generateID++;
 //		this._id = _generateID;
 		this._id = value;
 	}
 
+	/**
+	 * @return Trego flerën e ID-s
+	 */
 	protected int getId() {
 		return this._id;
 	}
 
+	/**
+	 * @param Merr flerën e Emrit ({@code String})
+	 */
 	protected void setEmri(String value) {
 		this._emri = value;
 	}
 
+	/**
+	 * @return Trego flerën e Emrit
+	 */
 	protected String getEmri() {
 		return this._emri;
 	}
 
+	/**
+	 * @param Merr flerën e Mbiemrit ({@code String}
+	 */
 	protected void setMbiemri(String value) {
 		this._mbiemri = value;
 	}
 
+	/**
+	 * @return Trego flerën e Mbiemrit
+	 */
 	protected String getMbiemri() {
 		return this._mbiemri;
 	}
 
-	protected String getEmriPrindit() {
-		return this._emriPrindit;
-	}
-
+	/**
+	 * @param Merr flerën e Emrit të prindit ({@code String}
+	 */
 	protected void setEmriPrindit(String value) {
 		this._emriPrindit = value;
 	}
 
+	/**
+	 * @return Trego fleren e Emrit të prindit
+	 */
+	protected String getEmriPrindit() {
+		return this._emriPrindit;
+	}
+
+	/**
+	 * @param Merr flerën e Drejtimit ({@code String}
+	 */
 	protected void setDrejtimi(String value) {
 		this._drejtimi = value;
 	}
 
+	/**
+	 * @return Trego fleren e Emrit të prindit
+	 */
 	protected String getDrejtimi() {
 		return this._drejtimi;
 	}
 
-	protected String getEmail() {
-		return this._email;
-	}
-
+	/**
+	 * @param Merr flerën e Emailit ({@code String})
+	 */
 	protected void setEmail(String value) {
 		this._email = value;
 	}
 
-	protected String getNumber() {
-		return this._numriTel;
+	/**
+	 * @return Trego fleren e Emilit
+	 */
+	protected String getEmail() {
+		return this._email;
 	}
 
+	/**
+	 * @param Trego fleren e Emrit të prindit ( {@code String})
+	 */
 	protected void setNumber(String value) {
 		this._numriTel = value;
 	}
 
+	/**
+	 * @return Trego fleren e Numrit
+	 */
+	protected String getNumber() {
+		return this._numriTel;
+	}
+
+	/**
+	 * @param Merr fleren notes mesatare ({@code float})
+	 */
 	protected void setNotaMesatare(float value) {
 		this._notaMesatare = value;
 	}
 
+	/**
+	 * @return Trego fleren e Notes Mesatare
+	 */
 	protected double getNotaMesatare() {
 		return this._notaMesatare;
 	}
